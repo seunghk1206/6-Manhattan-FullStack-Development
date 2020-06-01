2389a84a91c07cbaeb019ebfa32c30f72c956f90
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-06-01 12:38:03 +0900

    Update reader.py

--- newspaper_reader/reader.py
+++ newspaper_reader/reader.py
@@ -1,35 +1,56 @@
+import itertools
 paper = open("./newspaper_reader/eng_news_2016_10K-sentences.txt", "r", encoding = "utf8")
-# "r", UTF-8
+# "r", UTF-8,, ./newspaper_reader/eng_news_2016_10K-sentences.txt
 no_punct = ""
 Dict = {}
 Lolines = []
 noPuncList = []
+tempL = []
+Ln2 = []
 #dict
-def PuncRemove(st):
-    punctuations = '''!@#$%^&*()_-=+[]:;"'<>,./?'''
-    for i in st.lower():
-        if i in punctuations:
-            st = st.replace(i, " ")
-    return st
+def replace(listAccept, listRecieve, whichOne):
+    for each in listAccept:
+        listRecieve.append(each.split(whichOne))
+    listAccept.clear()
+    dummy = list(itertools.chain(*listRecieve))
+    listRecieve.clear()
+    for i in dummy:
+        listRecieve.append(i)
 for i in range(10000):
     lines = paper.readline(i)
     Lolines.append(lines)
-for i in range(len(Lolines)):
-    noPuncList.append(PuncRemove(Lolines[i]))
-noIntList = []
-dsd = ''.join([i for i in noPuncList if not i.isdigit()])
-noIntList.append(dsd)
-noNewLineList = []
-for each in noIntList:
-    noNewLineList.append(each.split('\t'))
-for each in noNewLineList:
-    noNewLineList[noNewLineList.index(each)] = noNewLineList[noNewLineList.index(each)].split('\n')
-for each in range(len(noNewLineList)):
-    dummy = list(noNewLineList[each].split(" "))
-    for eachWord in dummy:
-        try:
-            Dict[eachWord] += 1
-        except:
-            Dict[eachWord] = 1
+replace(Lolines, tempL, '\t')
+replace(tempL, Lolines, '\n')
+replace(Lolines, tempL, ',')
+replace(tempL, Lolines, '.')
+replace(Lolines, tempL, '-')
+replace(tempL, Lolines, ':')
+replace(Lolines, tempL, ';')
+replace(tempL, Lolines, '1')
+replace(Lolines, tempL, '2')
+replace(tempL, Lolines, '3')
+replace(Lolines, tempL, '4')
+replace(tempL, Lolines, '5')
+replace(Lolines, tempL, '6')
+replace(tempL, Lolines, '7')
+replace(Lolines, tempL, '8')
+replace(tempL, Lolines, '9')
+replace(Lolines, tempL, ')')
+replace(tempL, Lolines, '(')
+replace(Lolines, tempL, '@')
+replace(tempL, Lolines, '"')
+replace(Lolines, tempL, "'")
+replace(tempL, Lolines, '~')
+replace(Lolines, tempL, '`')
+replace(tempL, Lolines, '?')
+for each in range(len(Lolines)):
+    Ln = list(Lolines[each].split(" "))
+    for each in Ln:
+        Ln2.append(each.lower())
+for eachWord in Ln2:
+    try:
+        Dict[eachWord] += 1
+    except:
+        Dict[eachWord] = 1
 print(Dict)
 # readline
