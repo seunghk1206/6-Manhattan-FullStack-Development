42a9e61affc168e8f7b7838aaf201d7aec4741f9
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-06-07 14:15:44 +0900

    Update question#8.py

--- questions/question#8.py
+++ questions/question#8.py
@@ -1,12 +1,29 @@
 a = int(input())
 LoP = []
+LoP2 = []
+countA = 0
+countB = 0
 for i in range(a):
     x = input()
     LoP.append(x)
+
 for each in range(a):
-    x = LoP[each].count('(')
-    y = LoP[each].count(')')
-    if x == y:
-        print("YES")
-    else:
+    if LoP[each][0] == ')':
         print("NO")
+    else:
+        for i in LoP[each]:
+            if i == '(':
+                try:
+                    LoP.remove(i)
+                    LoP.remove(')')
+                except:
+                    if len(LoP) == 0:
+                        print("YES")
+                    else:
+                        print("NO")
+                    break
+#
+'''
+))((()))
+()(())
+'''
