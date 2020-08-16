28c33444c23b773741329a3b63978e8537cf0fa4
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-08-16 22:11:11 +0900

    Update data_cleansing.py

--- CNNproject/data_cleansing.py
+++ CNNproject/data_cleansing.py
@@ -1,6 +1,29 @@
 def chain2D(L):
     return [eachOb for each in L for eachOb in each]
+def splitran(L, freq):
+    run = True
+    tempL = []
+    ListA = []
+    ListB = []
+    n = 0
+    for each in L:
+        if '\n' in each:
+            if each.split('\n') == ['']:
+                pass
+            else: 
+                tempL.append(each.split('\n')[0])
+        elif '\n' not in each:
+            tempL.append(each)
+    for _ in range(tempL.count('')):
+        tempL.remove('')
+    for each in range(len(tempL)//freq):
+        for eachInd in range(freq):
+            ListB.append(tempL[eachInd+each*freq])
+        ListA.append(ListB)
+        ListB = []
+    return ListA
 DataPaper = open("C:/Users/seunghyeon/Documents/GitHub/python_enhanced/CNNproject/datasets_228_482_diabetes.txt", "r", encoding = "utf8")
-chainedL = chain2D([each.split(',') for each in [DataPaper.readline(each) for each in range(768)]])
+splitL = splitran(chain2D([each.split(',') for each in [DataPaper.readline(each) for each in range(768)]]), 9)
 #Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age,Outcome
 #9
+print(splitL)
