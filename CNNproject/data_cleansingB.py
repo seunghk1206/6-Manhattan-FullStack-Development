56e7a9ef87a012472ef9cd5625f1522c5a16b988
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-09-24 13:44:58 +0900

    Update data_cleansingB.py

--- CNNproject/data_cleansingB.py
+++ CNNproject/data_cleansingB.py
@@ -1,5 +1,5 @@
-DataPaper = open("C:/Users/seunghyeon/Documents/GitHub/python_enhanced/CNNproject/datasets_228_482_diabetes.txt", "r", encoding = "utf8")
-splitL = [[DataPaper.readline(each)] for each in range(768)]
+DataPaper = open("datasets_228_482_diabetes.txt", "r", encoding = "utf8")
+splitL = [[float(a) for a in DataPaper.readline(each)] for each in range(768)]
 print(splitL)
 #Pregnancies = [each[0] for each in splitL]
 #Glucose = [each]
